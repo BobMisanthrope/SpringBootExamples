2674c116a2650f13460eb229e919d4ca4f2b04ac
BobMisanthrope <vladimirdolotov@mail.ru>
2024-11-12 14:09:06 +0300

    Initial version

---
+++ src/test/java/com/vd/sbOrganizer/SbOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vd.sbOrganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
